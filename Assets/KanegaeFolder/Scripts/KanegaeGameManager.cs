a78baf91b82cc43b49ab14f8225ef3d144379cd9
KaneShoma <kane.shoma37@gmail.com>
2021-02-10 18:37:13 +0900

    シーン遷移(仮)を一通り完成させました(鐘ヶ江)

---
+++ Assets/KanegaeFolder/Scripts/KanegaeGameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KanegaeGameManager : MonoBehaviour
+{
+    public GameObject Pose;
+    [SerializeField]
+    static public bool OnPose = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OnPose == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                OnPose = true;
+                Pose.SetActive(true);
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                OnPose = false;
+                Pose.SetActive(false);
+            }
+        }
+    }
+}
