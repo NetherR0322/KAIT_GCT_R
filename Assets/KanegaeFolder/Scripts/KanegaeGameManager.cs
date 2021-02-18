2b58593192cdea5dc7efe375001a6b5e303522f7
KaneShoma <kane.shoma37@gmail.com>
2021-02-18 19:22:01 +0900

    貝関係のスクリプトを書きました(鐘ヶ江)

--- Assets/KanegaeFolder/Scripts/KanegaeGameManager.cs
+++ Assets/KanegaeFolder/Scripts/KanegaeGameManager.cs
@@ -1,20 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class KanegaeGameManager : MonoBehaviour
 {
     public GameObject Pose;
+
+    public static int score;
+
+    public static bool checkScore = false;
+
+    public TextMeshProUGUI ScoreText;
+
     [SerializeField]
     static public bool OnPose = false;
     void Start()
     {
-        
+        score = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (checkScore == true)
+        {
+           // Debug.Log("trueになったのでスコア足します");
+            //score += 10;
+            Debug.Log("Score:" + score);
+        }
+       
+
         if (OnPose == false)
         {
             if (Input.GetKeyDown(KeyCode.Escape))
@@ -31,5 +47,7 @@ public class KanegaeGameManager : MonoBehaviour
                 Pose.SetActive(false);
             }
         }
+
+        ScoreText.text = "Score:"+score;
     }
 }
---
+++ Assets/KanegaeFolder/Scripts/ShellfishScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellfishScript : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("当たりました");
+        if (collision.gameObject.tag == "tako")
+        {
+            KanegaeGameManager.checkScore = true;
+            KanegaeGameManager.score += 10;
+            Debug.Log("Score:" + KanegaeGameManager.score);
+            Debug.Log("trueにしました");
+            Destroy(this.gameObject);
+            KanegaeGameManager.checkScore = false;
+
+            Debug.Log("falseにしました");
+
+        }
+    }
+}
