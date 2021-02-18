5f29c6e16601e96005aac9118190f53c0e418a48
KaneShoma <kane.shoma37@gmail.com>
2021-02-19 03:45:31 +0900

    制限時間をリザルトシーンに表示、水たまりでの制限時間の回復の処理のスクリプトを書きました(鐘ヶ江)

---
+++ Assets/KanegaeFolder/Scripts/LimitScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class LimitScript : MonoBehaviour
+{
+    [SerializeField]
+    public static float limit = 60.0f;
+
+    public TextMeshProUGUI LimitText;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        limit -= Time.deltaTime;
+
+        LimitText.text = limit.ToString("f2") + "sec";
+
+        if (limit <= 0)
+        {
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+}
