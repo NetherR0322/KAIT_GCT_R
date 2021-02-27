cbab97db74159d1e0f97ca1780a2c0d32c74aff9
NetherR <rin.12.0322@gmail.com>
2021-02-27 16:43:34 +0900

    通信削減ちゃれんじ

--- Assets/RinFolder/Scripts/takoTransform.cs
+++ Assets/RinFolder/Scripts/takoTransform.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using Photon.Pun;
 public class takoTransform : MonoBehaviourPunCallbacks
 {
+    Vector3 nowPos;
+    Vector3 beforePos;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,14 +15,16 @@ public class takoTransform : MonoBehaviourPunCallbacks
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = this.transform.position;
-        if(LobbyNetwork.id==1)GetComponent<PhotonView>().RPC(nameof(TransformSync), RpcTarget.All,pos);
+        beforePos = nowPos;
+        nowPos = this.transform.position;
+        //if(LobbyNetwork.id==1)
+        if(nowPos!=beforePos)GetComponent<PhotonView>().RPC(nameof(TransformSync), RpcTarget.All,nowPos);
 
     }
     [PunRPC]
     private void TransformSync(Vector3 pos)
     {
         this.transform.position = pos;//IKの位置をカーソルの位置に持っていく
-
+        Debug.Log("takoTransform>PUN通信の実行");
     }
 }
