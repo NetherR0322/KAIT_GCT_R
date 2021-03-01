51f39229c70fafd9e3db13f98bbfa72a143899ed
UHOUHOUHOUHO <gurenagaoredare@gmail.com>
2021-03-01 23:07:08 +0900

    仮グラフィックでチュートリアルを作成(途中まで)(猿渡)

---
+++ Assets/SawatariFolder/Tutrial/Script/JumpTutrialScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpTutrialScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("すり抜けている");
+        if (other.gameObject.tag == "tako")
+        {
+            Debug.Log("タコがコンロのトリガーをすり抜けている");
+            GMScript.Movetutrial = false;
+            GMScript.Jumptutrial = true;
+        }
+    }
+}
---
+++ Assets/SawatariFolder/Tutrial/Script/TimelinePlayer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelinePlayer : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+
+    void Start()
+    {
+        //同じゲームオブジェクトにあるPlayableDirectorを取得する
+        playableDirector = GetComponent<PlayableDirector>();
+        PlayTimeline();
+    }
+    private void Update()
+    {
+        if (GMScript.Movetutrial == true&&GMScript.gamestate==0) { 
+        }
+    }
+
+    //再生する
+    void PlayTimeline()
+    {
+        playableDirector.Play();
+    }
+
+    //一時停止する
+    void PauseTimeline()
+    {
+        playableDirector.Pause();
+    }
+
+    //一時停止を再開する
+    void ResumeTimeline()
+    {
+        playableDirector.Resume();
+    }
+
+    //停止する
+    void StopTimeline()
+    {
+        playableDirector.Stop();
+    }
+
+}
