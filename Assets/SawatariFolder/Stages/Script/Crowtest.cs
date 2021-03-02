3b55c89afcf157f643f5f27e821207d11c56e71b
UHOUHOUHOUHO <gurenagaoredare@gmail.com>
2021-03-02 16:38:20 +0900

    商店街ステージの調整を行いました(猿渡)

---
+++ Assets/SawatariFolder/Stages/Script/Crowtest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crowtest : MonoBehaviour
+{
+    float Times;
+    public float Xsize = 1.494188f;
+    public float Ysize = 1.494188f;
+    private Vector3 targetps;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Return")
+        {
+            Vector3 crow = new Vector3(Xsize, Ysize, 1);
+            crow.x = crow.x * -1;
+            Xsize = Xsize * -1;
+            gameObject.transform.localScale = crow;
+            Debug.Log("OK!!");
+        }
+    }
+}
--- Assets/SawatariFolder/Tutrial/Script/TimelinePlayer.cs
+++ Assets/SawatariFolder/Tutrial/Script/TimelinePlayer.cs
@@ -11,32 +11,32 @@ public class TimelinePlayer : MonoBehaviour
         playableDirector = GetComponent<PlayableDirector>();
         PlayTimeline();
     }
-    private void Update()
+    void Update()
     {
         if (GMScript.Movetutrial == true&&GMScript.gamestate==0) { 
         }
     }
 
     //再生する
-    void PlayTimeline()
+    public void PlayTimeline()
     {
         playableDirector.Play();
     }
 
     //一時停止する
-    void PauseTimeline()
+    public void PauseTimeline()
     {
         playableDirector.Pause();
     }
 
     //一時停止を再開する
-    void ResumeTimeline()
+    public void ResumeTimeline()
     {
         playableDirector.Resume();
     }
 
     //停止する
-    void StopTimeline()
+    public void StopTimeline()
     {
         playableDirector.Stop();
     }
