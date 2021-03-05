0f937457a4f6729a33038e34f8cd3dbd365ff1a7
NetherR <rin.12.0322@gmail.com>
2021-03-06 01:12:12 +0900

    BGMのしすてっむ追加

---
+++ Assets/RinFolder/Scripts/activeSceneChanger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class activeSceneChanger : MonoBehaviour
+{
+    public string sceneName;
+    BGMPlayer script;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Scene scene = SceneManager.GetSceneByName(sceneName);
+        SceneManager.SetActiveScene(scene);
+        script = GameObject.Find("BGMPlayer").GetComponent<BGMPlayer>();
+        script.BgmChangeTester();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
