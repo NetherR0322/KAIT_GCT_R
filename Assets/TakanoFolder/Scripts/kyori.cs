feb62152ef062c9dbe33cc0c286354c7f7619168
tmshr <58304948+tmshr@users.noreply.github.com>
2021-06-13 09:49:22 +0900

    時間修正、距離追加

---
+++ Assets/TakanoFolder/Scripts/kyori.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class kyori : MonoBehaviour
+{
+    public Text distance;
+    public Transform tako;
+    public Transform goal;
+    float _distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _distance = Vector3.Distance(tako.position, goal.position);
+        distance.text = "海まであと"+_distance.ToString("f0")+"m";
+    }
+}
