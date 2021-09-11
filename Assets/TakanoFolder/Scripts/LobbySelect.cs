7dd140af57bccebe88df7a21357e83f5d12a2103
tmshr <58304948+tmshr@users.noreply.github.com>
2021-09-11 13:27:04 +0900

    ルーム作成/参加の画面を分割

---
+++ Assets/TakanoFolder/Scripts/LobbySelect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbySelect : MonoBehaviour
+{
+    public GameObject CreateRoomLobby;
+    public GameObject SearchRoomLobby;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void On_click_CreateRoomLobby()
+    {
+        BGMPlayer.GetInstance().PlaySound(0);
+        SearchRoomLobby.SetActive(false);
+        CreateRoomLobby.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+    public void On_click_SearchRoomLobby()
+    {
+        BGMPlayer.GetInstance().PlaySound(0);
+        CreateRoomLobby.SetActive(false);
+        SearchRoomLobby.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
