eb820915e109319b5343d4d944fcbb0b5d5ffc15
tmshr <58304948+tmshr@users.noreply.github.com>
2021-09-12 12:21:27 +0900

    分けたルーム作成/参加の画面を統一に戻しました

---
+++ Assets/TakanoFolder/Scripts/OpenCreateWindow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCreateWindow : MonoBehaviour
+{
+    public GameObject CreateWindow;
+
+    public void On_click_OpenCWindow()
+    {
+        CreateWindow.SetActive(true);
+    }
+    public void On_click_CloseCWindow()
+    {
+        CreateWindow.SetActive(false);
+    }
+}
