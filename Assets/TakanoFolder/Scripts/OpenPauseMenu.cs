630d5a80a2a5235b08d07b2243d3b9e03cab75ef
tmshr <58304948+tmshr@users.noreply.github.com>
2021-09-17 10:54:20 +0900

    UI修正,ポーズボタン追加

---
+++ Assets/TakanoFolder/Scripts/OpenPauseMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenPauseMenu : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    public GameObject pauseBotton;
+    public GameObject grayOut;
+
+    public void On_click_OpenPmenu()
+    {
+        BGMPlayer.GetInstance().PlaySound(0);
+        PauseMenu.SetActive(true);
+        pauseBotton.SetActive(false);
+        grayOut.SetActive(true);
+    }
+    public void On_click_CloseCPmenu()
+    {
+        BGMPlayer.GetInstance().PlaySound(0);
+        PauseMenu.SetActive(false);
+        pauseBotton.SetActive(true);
+        grayOut.SetActive(false);
+    }
+}
