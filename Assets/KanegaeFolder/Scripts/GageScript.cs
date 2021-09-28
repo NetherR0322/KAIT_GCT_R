feda7c63eea399ca50981d01ac2b283870ecec07
KaneShoma <kane.shoma37@gmail.com>
2021-09-29 00:03:09 +0900

    ゲージのバグ修正(鐘ヶ江)

--- Assets/KanegaeFolder/Scripts/GageScript.cs
+++ Assets/KanegaeFolder/Scripts/GageScript.cs
@@ -7,32 +7,48 @@ public class GageScript : MonoBehaviour
     [SerializeField]
     public float speed = 10f;
 
+    private float count = 0f;
+
     private float dist = 0f;
 
     public GameObject GageTako;
 
     void Start()
     {
-        //   dist = Kyorihakaru.dis;
+    
+           dist = Kyorihakaru.dis;
     }
 
     // Update is called once per frame
     void Update()
     {
+        count += Time.deltaTime;
+        Debug.Log(Kyorihakaru.dis);
+        Debug.Log(dist);
 
-        Debug.Log(GageTako.transform.localPosition.y);
-
-        if (dist!= Kyorihakaru.dis)
+        if (count > 9.0f)
         {
-            if (dist > Kyorihakaru.dis)
+
+            if (Kyorihakaru.dis == dist)
             {
-                GageTako.transform.localPosition = new Vector3(GageTako.transform.localPosition.x, GageTako.transform.localPosition.y + speed, GageTako.transform.localPosition.z);
+                GageTako.transform.localPosition = new Vector3(GageTako.transform.localPosition.x, GageTako.transform.localPosition.y, GageTako.transform.localPosition.z);
                 dist = Kyorihakaru.dis;
             }
-            else
+
+
+            if (dist != Kyorihakaru.dis)
             {
-                GageTako.transform.localPosition = new Vector3(GageTako.transform.localPosition.x, GageTako.transform.localPosition.y - speed, GageTako.transform.localPosition.z);
-                dist = Kyorihakaru.dis;
+
+                if (dist > Kyorihakaru.dis)
+                {
+                    GageTako.transform.localPosition = new Vector3(GageTako.transform.localPosition.x, GageTako.transform.localPosition.y + speed, GageTako.transform.localPosition.z);
+                    dist = Kyorihakaru.dis;
+                }
+                else
+                {
+                    GageTako.transform.localPosition = new Vector3(GageTako.transform.localPosition.x, GageTako.transform.localPosition.y - speed, GageTako.transform.localPosition.z);
+                    dist = Kyorihakaru.dis;
+                }
             }
         }
 
